77860da8e782e40aab02dfb0460fdbc725312ea3
TEFTE1KA1 <j21nikitunm@gmail.com>
2026-08-01 15:08:43 +0500

    index.js

--- bot/index.js
+++ bot/index.js
@@ -1,6 +1,6 @@
 const https = require('https');
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
     const userMessage = req.query.msg; 
     if (!userMessage) return res.send('Задай вопрос! Пример: !ai привет');
 
@@ -16,7 +16,6 @@ module.exports = async (req, res) => {
         response.on('end', () => {
             let aiText = data.trim();
             
-            // Если ответ от ИИ слишком длинный, принудительно обрезаем под лимиты Найтбота
             if (aiText.length > 250) {
                 aiText = aiText.substring(0, 247) + "...";
             }
@@ -27,4 +26,4 @@ module.exports = async (req, res) => {
     }).on("error", (err) => {
         res.send("ИИ задумался, повтори вопрос через 5 секунд!");
     });
-};
+}
